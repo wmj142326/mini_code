d661bd8e647671b9201ab77def2dd516699c5b04
wangmeijun <wmj142326@163.com>
2022-11-29 23:20:45 +0800

    add image_compare_2

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 1.`video_compare`:视频拼接比较可视化，[BiLibili观看](https://www.bilibili.com/video/BV1vP4y127gy/?spm_id_from=333.999.0.0&vd_source=1d5df1f3ef5b9d878809e5674ac44906) 
 
-2.`image_compare`:图片拼接比较可视化，[BiLibili观看](https://www.bilibili.com/video/BV1MR4y1Z7ZT/?vd_source=1d5df1f3ef5b9d878809e5674ac44906)
+2.`image_compare`:图片拼接比较可视化，[BiLibili观看](https://www.bilibili.com/video/BV1MR4y1Z7ZT/?vd_source=1d5df1f3ef5b9d878809e5674ac44906)  ；另外可以查看PPT的做法：[PPT制作图片矩阵](https://www.bilibili.com/video/BV1F14y1n7LF/)
+
+    image_compare_2: 新增一个make_grid()图像矩阵函数，类似于上面的PPT制作图像矩阵；
 
 3.`img_ext_tras`:
 图片后缀名无损转换，[BiLibili观看](https://www.bilibili.com/video/BV15B4y177Bi/?spm_id_from=333.999.0.0&vd_source=1d5df1f3ef5b9d878809e5674ac44906)
---
+++ image_compare/image_compare_2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/11/29
+# @Author : wmj
+# @Email : wmj142326@163.com
+# @File : image_compare.py
+# @Note : 图像拼接的一个新函数：from torchvision.utils import make_grid
+# ---------------------------
+import os
+import cv2
+import torchvision.utils
+from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
+from torchvision.utils import make_grid
+import torchvision.transforms as transforms
+
+
+def create_folder(folder):
+    """ Create a folder."""
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    return folder
+
+
+def get_img_list(img_folder):
+
+    img_path_list = os.listdir(img_folder)
+    img_path_list.sort(key=lambda i: int(i.split('.')[0]))
+
+    img_list = []
+
+    for img in tqdm(img_path_list):
+        img_path = os.path.join(img_folder, img)
+        image = cv2.imread(img_path)
+        cv2.putText(image, img, (50, 100), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
+        image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        trans = transforms.ToTensor()
+        image = trans(image)
+
+        img_list.append(image)
+    return img_list
+
+
+def main():
+    img_1 = "./img_1"
+    img_list = get_img_list(img_1)
+    img_out = make_grid(img_list, nrow=2, padding=10, pad_value=1)
+    torchvision.utils.save_image(img_out, "out.jpg")
+
+
+if __name__ == "__main__":
+    print("------------------------START------------------------")
+    main()
+    print("-------------------------END-------------------------")
