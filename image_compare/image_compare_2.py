5eb7d7a2a980bb7907d3d60aa9cbfc6ed78c0773
wangmeijun <wmj142326@163.com>
2022-11-29 23:37:41 +0800

    add image_compare_2

--- image_compare/image_compare_2.py
+++ image_compare/image_compare_2.py
@@ -22,9 +22,8 @@ def create_folder(folder):
 
 
 def get_img_list(img_folder):
-
     img_path_list = os.listdir(img_folder)
-    img_path_list.sort(key=lambda i: int(i.split('.')[0]))
+    img_path_list.sort(key=lambda i: int(i.split('.')[0]))  # 排序
 
     img_list = []
 
@@ -32,8 +31,8 @@ def get_img_list(img_folder):
         img_path = os.path.join(img_folder, img)
         image = cv2.imread(img_path)
         cv2.putText(image, img, (50, 100), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 2)
-        image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        trans = transforms.ToTensor()
+        image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  # cv转PIL
+        trans = transforms.ToTensor()  # PIL转Tensor
         image = trans(image)
 
         img_list.append(image)
@@ -43,7 +42,7 @@ def get_img_list(img_folder):
 def main():
     img_1 = "./img_1"
     img_list = get_img_list(img_1)
-    img_out = make_grid(img_list, nrow=2, padding=10, pad_value=1)
+    img_out = make_grid(img_list, nrow=3, padding=10, pad_value=1)
     torchvision.utils.save_image(img_out, "out.jpg")
 
 
